62ff1d8d818a6fb124e26f4155a2f7ef080206cb
정동원 <jdw1042710@gmail.com>
2024-03-17 18:44:54 +0900

    Initial commit

---
+++ Source/TacticalCombat/TacticalCombatGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TacticalCombatGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TACTICALCOMBAT_API ATacticalCombatGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
